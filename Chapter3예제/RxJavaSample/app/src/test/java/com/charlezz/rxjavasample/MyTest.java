5aed32ad2b19b92b0b89a54a4496c48c8c6547bc
Jongkook <conquerex@gmail.com>
2020-08-02 17:09:38 +0900

    chapter.3 간단히 Observable로 변환하기

--- Chapter3예제/RxJavaSample/app/src/test/java/com/charlezz/rxjavasample/MyTest.java
+++ Chapter3예제/RxJavaSample/app/src/test/java/com/charlezz/rxjavasample/MyTest.java
@@ -1,6 +1,12 @@
 package com.charlezz.rxjavasample;
 
 import org.junit.Test;
+import org.reactivestreams.Publisher;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import io.reactivex.rxjava3.core.Observable;
 
@@ -43,14 +49,54 @@ public class MyTest {
         source.subscribe(System.out::println);
     }
 
-    // ..... 여기까지 테스트 했음 ......
+    // 간단히 Observable로 변환하기.............
 
     @Test
     public void testFromArray() {
-        String[] itemArray = new String[]{"A", "B", "C"};
+        String[] itemArray = new String[]{"A", "B", "C..............."};
         Observable source = Observable.fromArray(itemArray);
         source.subscribe(System.out::println);
     }
-    
+
+    @Test
+    public void testFromIterable() {
+        ArrayList itemList = new ArrayList();
+        itemList.add("aa");
+        itemList.add("bbb");
+        itemList.add("cccc");
+        Observable source = Observable.fromIterable(itemList);
+        source.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testFromFuture() {
+        Future<String> future = Executors.newSingleThreadExecutor()
+                .submit(() -> {
+                    Thread.sleep(5000);
+                    return "Hello kooook";
+                });
+        Observable source = Observable.fromFuture(future);
+        source.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testFromPublisher() {
+        Publisher<String> publisher = subscriber -> {
+            subscriber.onNext("1111");
+            subscriber.onNext("2222");
+            subscriber.onNext("3333");
+        };
+        Observable source = Observable.fromPublisher(publisher);
+        source.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testFromCallable() {
+        Callable<String> callable = () -> "Hello World";
+        Observable source = Observable.fromCallable(callable);
+        source.subscribe(System.out::println);
+    }
+
+    //.......
 
 }
