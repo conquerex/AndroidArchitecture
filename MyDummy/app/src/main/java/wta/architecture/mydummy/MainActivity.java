8e8ecd3ce29c69629e78fea4ddbbf41537cc99c6
Jongkook <conquerex@gmail.com>
2020-08-19 15:55:00 +0900

    Mydummy 프로젝트 설정하기

--- MyDummy/app/src/main/java/wta/architecture/mydummy/MainActivity.java
+++ MyDummy/app/src/main/java/wta/architecture/mydummy/MainActivity.java
@@ -1,6 +1,7 @@
 package wta.architecture.mydummy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
